c58e45b8ac417658eb488e55a9a1dfea2371cac8
Adam Roehrig <arroeh01@outlook.com>
2026-04-25 17:50:42 -0400

    Setting api versions better with constants

--- Demo.Restuarants.API.Models/ApiVersions.cs
+++ Demo.Restuarants.API.Models/ApiVersions.cs
@@ -2,16 +2,16 @@
 
 public static class ApiVersions
 {
-    public const string Latest = "3.0";
-    public const string QueryParamVersion = "?api-version=3.0";
+    public const string Latest = V3;
+    public const string QueryParamVersion = $"?api-version={Latest}";
 
     public const string V1 = "1.0";
     public const string V2 = "2.0";
     public const string V3 = "3.0";
 
     public static string[] AllVersions => [
-        "1.0",
-        "2.0",
-        "3.0"
+        V1,
+        V2,
+        V3
     ];
 }
